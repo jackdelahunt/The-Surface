814a305419f7a89a98c31c881ad271dde3dec1c1
jackdelahunt <45426048+jackdelahunt@users.noreply.github.com>
2020-10-22 11:38:19 +0100

    added starting point for building manager

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("tick", 0f, 1f);
+    }
+
+    public void tick() {
+        print("Tick");
+    }
+
+}
