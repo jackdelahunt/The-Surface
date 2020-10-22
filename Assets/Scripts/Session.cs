148dfff9b2c0b090df27e5f64c6ae3af88ec6805
jackdelahunt <45426048+jackdelahunt@users.noreply.github.com>
2020-10-22 14:35:00 +0100

    added splash screen

---
+++ Assets/Scripts/Session.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Session : MonoBehaviour
+{
+    public void loadNext() {
+        SceneManager.LoadScene("Mission Control");
+    }
+}
