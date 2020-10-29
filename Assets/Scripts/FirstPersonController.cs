31c1e109334a1c8a164d6853a3b62924d7976e6c
jackdelahunt <45426048+jackdelahunt@users.noreply.github.com>
2020-10-29 11:05:19 +0000

    started day night cycle

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -40,7 +40,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private Vector3 m_OriginalCameraPosition;
         private float m_StepCycle;
         private float m_NextStep;
-        private bool m_Jumping;
+        [SerializeField]private bool m_Jumping;
         private AudioSource m_AudioSource;
 
         // Use this for initialization
