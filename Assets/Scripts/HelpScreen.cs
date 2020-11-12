01ef261a0881fda7747d80b91343a8310d0c01e5
Jack Delahunt <20085409@mail.wit.ie>
2020-11-12 14:48:51 +0000

    added help screen

---
+++ Assets/Scripts/HelpScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreen : MonoBehaviour
+{
+    [SerializeField]private Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void slideIn() {;
+        animator.SetTrigger("SlideIn");
+    }
+
+    public void slideOut() {
+        animator.SetTrigger("SlideOut");
+    }
+}
