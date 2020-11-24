de6255fb6b0b60519cb4f584c871c493093da848
jackdelahunt <20085409@mail.wit.ie>
2020-11-24 15:48:23 +0000

    options menu now saves values to playerPrefs

--- Assets/Scripts/OptionsScreen.cs
+++ Assets/Scripts/OptionsScreen.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsScreen : MonoBehaviour
 {
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Toggle AAToggle;
+    [SerializeField] private Toggle blurToggle;
     [SerializeField]private Animator animator;
 
 
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        resetOptionToPlayerPrefs();
     }
 
-    public void slideIn() {;
+    public void slideIn() {
+        resetOptionToPlayerPrefs();
         animator.SetTrigger("SlideIn");
     }
 
@@ -21,7 +29,17 @@ public class OptionsScreen : MonoBehaviour
         saveOptions();
 	}
 
-	public void saveOptions() {
+	public void resetOptionToPlayerPrefs() {
+		soundSlider.value = Settings.getSoundSetting();
+		musicSlider.value = Settings.getMusicSetting();
+        blurToggle.isOn = Settings.getBlurSetting() == 1 ? true : false;
+        AAToggle.isOn = Settings.getAASetting() == 1 ? true : false;
+    }
 
-	}
+	public void saveOptions() {
+        Settings.setSoundSetting(soundSlider.value);
+        Settings.setMusicSetting(musicSlider.value);
+        Settings.setBlurSetting(blurToggle.isOn ? 1 : 0);
+        Settings.setAASetting(AAToggle.isOn ? 1 : 0);
+    }
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Settings
+{
+	public static readonly string soundKey = "sound";
+	public static readonly string musicKey = "music";
+	public static readonly string blurKey = "blur";
+	public static readonly string AAKey = "antiAliasing";
+
+	public static float getSoundSetting() {
+		if (!PlayerPrefs.HasKey(soundKey))
+			PlayerPrefs.SetFloat(soundKey, 0.5f);
+
+		return PlayerPrefs.GetFloat(soundKey);	
+	}
+
+	public static float getMusicSetting() {
+		if (!PlayerPrefs.HasKey(musicKey))
+			PlayerPrefs.SetFloat(musicKey, 0.5f);
+
+		return PlayerPrefs.GetFloat(musicKey);
+	}
+	public static int getBlurSetting() {
+		if (!PlayerPrefs.HasKey(blurKey))
+			PlayerPrefs.SetInt(blurKey, 1);
+
+		return PlayerPrefs.GetInt(blurKey);
+	}
+
+	public static int getAASetting() {
+		if (!PlayerPrefs.HasKey(AAKey))
+			PlayerPrefs.SetInt(AAKey, 1);
+
+		return PlayerPrefs.GetInt(AAKey);
+	}
+
+	// ******************************************
+
+	public static void setSoundSetting(float value) {
+		// make sure it exists, not a great way 
+		getSoundSetting();
+		PlayerPrefs.SetFloat(soundKey, value);
+	}
+
+	public static void setMusicSetting(float value) {
+		// make sure it exists, not a great way 
+		getMusicSetting();
+		PlayerPrefs.SetFloat(musicKey, value);
+	}
+
+	public static void setBlurSetting(int value) {
+
+		if (value < 0 || value > 1)
+			return;
+
+		// make sure it exists, not a great way 
+		getBlurSetting();
+		PlayerPrefs.SetInt(blurKey, value);
+	}
+
+	public static void setAASetting(int value) {
+
+		if (value < 0 || value > 1)
+			return;
+
+		// make sure it exists, not a great way 
+		getAASetting();
+		PlayerPrefs.SetInt(AAKey, value);
+	}
+
+}
