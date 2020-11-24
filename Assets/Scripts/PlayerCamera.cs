5a902eb9119f16665604f3f50bf59bba510727df
jackdelahunt <20085409@mail.wit.ie>
2020-11-24 23:31:24 +0000

    antiAlias Setting now is working as intended

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private PostProcessLayer processLayer = null;
+
+	private void Start() {
+		processLayer = GetComponent<PostProcessLayer>();
+
+		if(processLayer != null) {
+			processLayer.antialiasingMode = Settings.getAASetting() ? PostProcessLayer.Antialiasing.FastApproximateAntialiasing : PostProcessLayer.Antialiasing.None;
+		}
+	}
+}
