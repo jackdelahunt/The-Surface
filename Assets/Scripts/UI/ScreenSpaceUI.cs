468d9bda34908a044c64ac0f11b430f4fe944148
jackdelahunt <20085409@mail.wit.ie>
2020-11-28 22:32:34 +0000

    added ship level requirment to complete the game

--- Assets/Scripts/UI/ScreenSpaceUI.cs
+++ Assets/Scripts/UI/ScreenSpaceUI.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScreenSpaceUI : MonoBehaviour
 {
     private Session session;
     [SerializeField]private GameObject pauseMenu;
+    [SerializeField] private TMP_Text currentLevelText;
 
     void Start() {
         session = GameObject.FindGameObjectWithTag("Session").GetComponent<Session>();
     }
     void Update() {
         pauseMenu.SetActive( session.getPauseState() );
+        currentLevelText.SetText( ResourceManager.getAmount("Ship Upgrade").ToString());
     }
 }
